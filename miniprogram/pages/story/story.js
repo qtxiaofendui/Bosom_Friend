9cfc5a4c3bf371e0591a852f2b30da373edc7663
3460741663 <3460741663@qq.com>
2019-08-23 09:57:41 +0800

    story更新

--- miniprogram/pages/story/story.js
+++ miniprogram/pages/story/story.js
@@ -7,10 +7,15 @@ const productsCollection =
 Page({
   
   data: {
-    images: [
-      'https://img.yzcdn.cn/vant/apple-1.jpg',
-      'https://img.yzcdn.cn/vant/apple-2.jpg'
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
     products: []
   },
   onLoad() {
