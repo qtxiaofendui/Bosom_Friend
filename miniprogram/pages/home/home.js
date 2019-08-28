babe907b8d7f1b543feb3eecd4336a8664f51f24
cysgg <279709624@qq.com>
2019-08-28 20:05:51 +0800

    home 下拉刷新

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -17,6 +17,7 @@ Page({
     duration: 500,
     danghangs:['评论','故事'],
     currentIndex : 1,
+    currentSongId : 0,
     showComments : false
   },
   watchtab(e){
@@ -55,26 +56,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    app.getWYYData('/comment/hot?id=186016&type=0')
+    this.initData()
+    
+    
+  },
+
+  getSongArr(){
+    return new Promise((resolve,reject)=>{
+      app.getWYYData(`/personalized/newsong`)
+      .then(res=>{
+        console.log(res);
+        return resolve(res)
+      })
+    })
+  },
+  initData(){
+    this.getSongArr().then(res=>[
+      this.setData({
+        songIdArr:res.data.result
+      })
+    ]).then(()=>{
+      let currentSongId = this.data.currentSongId
+      let songId = this.data.songIdArr[currentSongId].id
+      return app.getWYYData(`/comment/hot?id=${songId}&type=0`)
+    })
     .then(res=>{
+      let currentSongId = this.data.currentSongId
+      let songId = this.data.songIdArr[currentSongId].id
       console.log(res);
       let hotComs = res.data.hotComments
-
       let storys = []
-      hotComs.forEach(v=>{
-        let story = {}
-        story.id = v.commentId
-        story.name = v.user.nickname;
-        story.img = v.user.avatarUrl;
-        story.content = v.content;
-        let ti = new Date(v.time)
-        story.time = ti.toLocaleString();
-        story.zanimg = '/images/zan/zan_dark.png'
-        story.count = parseInt(Math.random()*1000)
-        story.hasActive = false
-        storys.push(story)
-      })
+      this.getStorys(hotComs,storys)
       this.setData({
+        songId : songId,
         hotComments : hotComs,
         storys : storys
       })
@@ -85,6 +99,21 @@ Page({
     let item_p = parseInt(100 / item_len)
     this.setData({
       item_p : item_p
+    })
+  },
+  getStorys(hots,storys){
+    hots.forEach(v=>{
+      let story = {}
+      story.id = v.commentId
+      story.name = v.user.nickname;
+      story.img = v.user.avatarUrl;
+      story.content = v.content;
+      let ti = new Date(v.time)
+      story.time = ti.toLocaleString();
+      story.zanimg = '/images/zan/zan_dark.png'
+      story.count = parseInt(Math.random()*1000)
+      story.hasActive = false
+      storys.push(story)
     })
   },
   /**
@@ -105,7 +134,21 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+    
+    
+  },
 
+  likeComments(){
+    let songId = this.data.songId
+    this.data.storys.forEach(v=>{
+      if(v.hasActive){
+        app.getWYYData(`/comment/like?id=${songId}&cid=${v.commentId}&t=1&type=0`)
+        .then(res=>{
+          console.log(res);
+          
+        })
+      }
+    })
   },
 
   /**
@@ -126,7 +169,27 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('到底了');
+    wx.showLoading({
+      title: '正在获取更多数据',
+      mask: true,
+    });
+    let currentSongId = ++this.data.currentSongId
+    this.setData({
+      currentSongId : currentSongId
+    })
+    let songId = this.data.songIdArr[currentSongId].id
+    app.getWYYData(`/comment/hot?id=${songId}&type=0`)
+    .then(res=>{
+      let hotComs = res.data.hotComments
+      let newHotStorys = []
+      this.getStorys(hotComs,newHotStorys);
+      let newStorys = this.data.storys.concat(newHotStorys);
+      this.setData({
+        storys : newStorys
+      })
+      wx.hideLoading();
+    })
   },
 
   /**
